3445c6fc237f949bdf84a323d11d34a58d520f57
Prajwal Bagewadi <77821176+prajwalbagewadi@users.noreply.github.com>
2024-12-02 10:02:49 +0530

    Create Hello.java

---
+++ JavaBasics/Hello.java
@@ -0,0 +1,27 @@
+package example;
+import java.lang.*;
+import java.util.Scanner;
+
+class Test{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		System.out.println("hello world");
+		System.out.println("Enter num for a=");
+		int a=sc.nextInt();
+		System.out.println("Enter num for b=");
+		int b=sc.nextInt();
+		int c=a+b;
+		System.out.println("Sum="+c);
+		//Arithmetic
+		c=a+b;
+		System.out.println("add="+c);
+		c=a-b;
+		System.out.println("sub="+c);
+		c=a*b;
+		System.out.println("mul="+c);
+		c=a/b;
+		System.out.println("div="+c);
+		c=a%b;
+		System.out.println("mod="+c);
+	}
+}
